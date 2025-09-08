d4b6b1280d1144297321740e1d282c8bb191586d
MarcoMambrettiUdesa <mmambretti@udesa.edu.ar>
2025-09-08 14:45:33 -0300

    corrigiendo cardstoprated

--- app-movies/src/components/CardsTopRated/CardsTopRated.js
+++ app-movies/src/components/CardsTopRated/CardsTopRated.js
@@ -16,7 +16,7 @@ class Cards extends Component {
 
   componentDidMount() {
 
-    fetch("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=0030cb6d5a827e996db3c37d4e1cadf3")
+    fetch("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200&api_key=0030cb6d5a827e996db3c37d4e1cadf3")
       .then(res => res.json())
       .then(data => this.setState({
         data: data.results,
