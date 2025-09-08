e63fa0aa6babfe7e25b06181e693093407d86fb0
valencag332 <vcagnolattimantaras@gmail.com>
2025-09-08 16:38:33 -0300

    pagina de error creada

--- app-movies/src/App.js
+++ app-movies/src/App.js
@@ -4,6 +4,9 @@ import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Home from './screens/Home/Home';
 import Resultados from './screens/Resultados/Resultados';
+import error from './screens/404/404';
+
+
 function App() {
 
 
@@ -14,7 +17,7 @@ function App() {
       <Switch>
         <Route path = '/' component = {Home} exact = {true} />
         <Route path = '/resultados/:query' component = {Resultados}/>
-
+        <Route path = '' component = {error}/>
 
       </Switch>  
 
---
+++ app-movies/src/screens/404/404.js
@@ -0,0 +1,5 @@
+function error() {
+    return(<h1>Error 404, pagina no encontrada</h1>)
+}
+
+export default error
